6c4808a8b7fa539b130bea64ef855cce4e585381
keniiy <kehindekehinde894@gmail.com>
2021-04-29 22:44:54 +0100

    final

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -6,13 +6,12 @@ import 'firebase/auth';
 // import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: "AIzaSyAw8tdo7IoVyJ30yHW95cPg7x7sl9t3vcU",
+  authDomain: "netflixclone-e9c26.firebaseapp.com",
+  projectId: "netflixclone-e9c26",
+  storageBucket: "netflixclone-e9c26.appspot.com",
+  messagingSenderId: "883917748477",
+  appId: "1:883917748477:web:19f011028c48c639472694"
 };
 
 const firebase = Firebase.initializeApp(config);
