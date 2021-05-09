160e3278ec5b1ae20e1a99b216478fab14c66348
keniiy <kehindekehinde894@gmail.com>
2021-05-09 11:05:29 +0100

    update

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components/macro';
-
+ 
 export const Container = styled.div`
   display: flex;
   border-bottom: 8px solid #222;
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -5,16 +5,15 @@ import 'firebase/auth';
 // 1) when seeding the database you'll have to uncomment this!
 // import { seedDatabase } from '../seed';
 
-const config = {
+
+const firebase = Firebase.initializeApp({
   apiKey: "AIzaSyAw8tdo7IoVyJ30yHW95cPg7x7sl9t3vcU",
   authDomain: "netflixclone-e9c26.firebaseapp.com",
   projectId: "netflixclone-e9c26",
   storageBucket: "netflixclone-e9c26.appspot.com",
   messagingSenderId: "883917748477",
   appId: "1:883917748477:web:19f011028c48c639472694"
-};
-
-const firebase = Firebase.initializeApp(config);
+});
 // 2) when seeding the database you'll have to uncomment this!
 // seedDatabase(firebase);
 // 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
