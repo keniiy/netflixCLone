c862b45f02a92922a59455a383a53ffb91a3886c
keniiy <kehindekehinde894@gmail.com>
2021-05-09 13:26:48 +0100

    commingsoon

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -102,7 +102,7 @@ export function BrowseContainer({ slides }) {
             <Card.Feature category={category}>
               <Player>
                 <Player.Button />
-                <Player.Video src="/videos/bunny.mp4" />
+                <Player.Video src="/videos/commingsoon.mp4" />
               </Player>
             </Card.Feature>
           </Card>
